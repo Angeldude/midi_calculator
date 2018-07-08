d090bd1349a103014b5fc06129e7efd3b5cf7af4
Angel Vanegas <angelman@gmail.com>
2018-07-08 17:35:59 -0400

    figured it out with flapjax; combining two inputs

--- src/module.js
+++ src/module.js
@@ -3,5 +3,16 @@ const mtof = (midiNum) => {
     return BASE_FREQ * Math.pow(2, (midiNum - 69)/12);
   }
 const midiFromString = R.compose(mtof, parseInt)
+
+const combineInputs = R.curry((x,y) => {       
+      const grr = parseInt(y);
+      const brr = parseInt(x);
+      return (grr+brr).toString();
+})
+
+const convertOctave = (x) =>{
+  const arg = parseInt(x);
+  return ((arg + 1) * 12);
+}
   
 const sanitizeFreq = num => isNaN(num) ? "Please enter a valid number" : num.toString() + " Hz"  
--- src/script.js
+++ src/script.js
@@ -1,11 +1,17 @@
 window.addEventListener('load', function(){
    let app = $('app');
-   let input = $('pitch');
+   let pitchName = $('letter');
+   let octave = $('octave');
+   
+   const pch = extractValueB(pitchName);
+   const oct = extractValueB(octave);
 
-   const formattedFreq = extractValueE(input)
-                      .mapE(midiFromString)
-                      .startsWith(NaN)
-                      .liftB(sanitizeFreq)
+   let pitches = pch.liftB(combineInputs);
+   let octaves = oct.liftB(convertOctave);
+
+   const formattedFreq = octaves.liftB(pitches)
+                         .liftB(midiFromString)
+                         .liftB(sanitizeFreq)
 
    let result  = DIV({id:'freq'}, P(formattedFreq));   
    app.appendChild(result)
